42d0683e4ed631f0e9b8ad6dd836b344e0e393dd
luke2327 <>
2023-03-17 17:17:43 +0900

    feat: add link

---
+++ page_repository/src/components/link/Link.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import LinkCard from '@/components/link/LinkCard';
+
+export default function Link() {
+  return (
+    <LinkWrapper>
+      <LinkHeading className={'link-heading'}>Link</LinkHeading>
+      <LinkCard />
+    </LinkWrapper>
+  );
+}
+
+const LinkWrapper = styled.div``;
+
+const LinkHeading = styled.h2`
+  font-family: var(--luke2327-fonts-heading);
+  font-weight: 400;
+  font-size: var(--luke2327-fontSizes-md);
+  line-height: 1.2;
+  margin-top: var(--luke2327-space-14);
+  margin-bottom: var(--luke2327-space-10);
+`;
