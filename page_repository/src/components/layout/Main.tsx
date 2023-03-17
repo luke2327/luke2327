821edc9f7966540b177848046c8245d805fde29d
luke2327 <>
2023-03-17 17:18:03 +0900

    feat: add main

--- page_repository/src/components/layout/Main.tsx
+++ page_repository/src/components/layout/Main.tsx
@@ -1,13 +1,15 @@
 import Description from '@/components/description/Description';
 import Experience from '@/components/experience/Experience';
 import Profile from '@/components/introduction/Profile';
+import Link from '@/components/link/Link';
 
 export default function Main() {
   return (
-    <div className={'mx-auto px-4 pt-28 sm:max-w-[36rem]'}>
+    <div className={'mx-auto px-4 py-28 sm:max-w-[36rem]'}>
       <Profile />
       <Description />
       <Experience />
+      <Link />
     </div>
   );
 }
