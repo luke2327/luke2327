343fd9d4aab6549192d19a8b62190d43f241dd81
luke2327 <>
2023-03-22 10:53:15 +0900

    feat: add external link

---
+++ page_repository/src/constants/externalLinks.ts
@@ -0,0 +1,24 @@
+export const extUrls = {
+  github: 'https://github.com/luke232',
+  etherscan:
+    'https://etherscan.io/address/0xd7346076Fc649A3E52E379F31d9c9Fb35f08d5Cb',
+  foundation: 'https://foundation.app/@Luke2327?tab=owned',
+};
+
+export const extLinks = [
+  {
+    head: 'GitHub',
+    url: extUrls.github,
+    title: '@luke2327',
+  },
+  {
+    head: 'Etherscan',
+    url: extUrls.etherscan,
+    title: 'luke2327.eth',
+  },
+  {
+    head: 'Foundation',
+    url: extUrls.foundation,
+    title: '@luke2327',
+  },
+];
