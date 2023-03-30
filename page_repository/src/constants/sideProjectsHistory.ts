791812a7482c9f008c9c33d903b6f3d7ec3001b1
luke2327 <>
2023-03-30 14:38:13 +0900

    feat: add side project

--- page_repository/src/constants/sideProjectsHistory.ts
+++ page_repository/src/constants/sideProjectsHistory.ts
@@ -3,27 +3,54 @@ import type { History } from '@/constants/experienctHistory';
 export const spHistory = [
   {
     title: {
+      en: 'Chocolat',
+    },
+    description: {
+      en: `A letter to a precious person`,
+      ko: '',
+      ja: '',
+    },
+    link: 'https://chocolat-frontend.vercel.app',
+    period: '2022 - present',
+    skill: ['TS', 'Next', 'Tailwind', 'Express', 'Sequelize', 'RDS', 'ECS'],
+  },
+  {
+    title: {
+      en: 'Legion solver',
+    },
+    description: {
+      en: `MapleStory's complex union system solved at once`,
+      ko: '',
+      ja: '',
+    },
+    link: 'https://xenogents.github.io/LegionSolver',
+    period: '2021 - 2021',
+    skill: ['HTML', 'CSS'],
+  },
+  {
+    title: {
       en: 'Play with me',
     },
     description: {
-      en: `Application that game...`,
+      en: `Let's play a fun game together`,
       ko: '',
       ja: '',
     },
     period: '2020 - 2021',
-    skill: ['TS', 'MobX', 'Express'],
+    skill: ['TS', 'Next', 'Serverless', 'MySQL', 'WebSocket'],
   },
   {
     title: {
       en: 'LMS',
     },
     description: {
-      en: `Application that management university student report...`,
+      en: `Application that college student assignment management`,
       ko: '',
       ja: '',
     },
     period: '2020 - 2021',
-    skill: ['TS', 'MobX', 'Express'],
+    link: 'http://ec2-3-37-36-193.ap-northeast-2.compute.amazonaws.com:3000',
+    skill: ['TS', 'React', 'MaterialUI', 'Express'],
   },
   {
     title: {
