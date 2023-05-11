03d8176c5a2ceea7ec8f3a8b1b1e007af617f163
luke2327 <>
2023-05-11 17:56:07 +0900

    feat: add ga

--- page_repository/src/constants/AppConfig.ts
+++ page_repository/src/constants/AppConfig.ts
@@ -4,3 +4,4 @@ export const languages: Array<SupportedLanguage> = ['ja', 'ko', 'en'];
 export const defaultLanguage = 'en';
 
 export const defaultThemeColor = 'dark';
+export const gaTrackingId = 'G-63QM82SWBB';
