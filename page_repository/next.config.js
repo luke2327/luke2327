1d1e33c3f00403b83512e94b7f9e575ceb4968e8
luke2327 <>
2023-05-14 00:29:33 +0900

    feat: mod nextconfig

--- page_repository/next.config.js
+++ page_repository/next.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'kr', 'jp'],
+    defaultLocale: 'en-US',
+    domains: [
+      {
+        domain: 'resume.malihua.jp',
+        defaultLocale: 'en-US',
+      },
+    ],
+  },
 };
