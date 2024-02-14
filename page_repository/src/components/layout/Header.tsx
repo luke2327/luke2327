f00b67045ac42932ee40a4508cf81631cd930a5e
luke2327 <qwepoi8526@gmail.com>
2024-02-14 20:29:46 +0900

    페이지 레이아웃 및 내비게이션 변경

--- page_repository/src/components/layout/Header.tsx
+++ page_repository/src/components/layout/Header.tsx
@@ -1,17 +1,33 @@
+import clsx from "clsx";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const location = [
+  {
+    label: '/',
+    path: '/'
+  }, {
+    label: 'Portfolio',
+    path: '/portfolio'
+  }
+]
+
 export default function Header() {
+  const { pathname } = useRouter();
+  console.log(pathname)
   return (
     <header
       className={'fixed top-0 flex w-full items-center justify-between p-4'}
     >
-      <div className={'flex items-center'}>
-        <a
-          href={'#main'}
-          aria-label={`nickname: Liam, scroll to top`}
-          aria-live="polite"
-        >
-          Liam
-        </a>
-      </div>
+      <nav className={'flex items-center gap-4'}>
+        {
+          location.map((item, idx) => (
+            <Link key={idx} href={item.path} className={clsx(pathname === item.path ? 'text-[#fff]' : '', 'px-2')}>
+              {item.label}
+            </Link>
+          ))
+        }
+      </nav>
       <div className={'flex items-center justify-between gap-4'}>
         {/* <Languages strokeWidth={1.5} /> */}
         {/* <LanguageSelect />
