bc8333dc3f4fba97c4bef253fcff52a16c44c622
luke2327 <qwepoi8526@gmail.com>
2026-01-15 11:42:04 +0900

    update: tsconfig and next config

--- page_repository/next.config.js
+++ page_repository/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: true,
-  reactStrictMode: true,
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
